2c4ea0a71500987a0cdc8e64733ddd53a471620c
Multimedia-Bjarke <bjarkedtholm@gmail.com>
2025-09-01 09:17:47 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import { useState, useEffect } from "react";
-import User from "./components/User";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  async function fetchUsers() {
-    const response = await fetch("https://raw.githubusercontent.com/cederdorff/race/master/data/users.json"); // fetch the data from the API
-    const data = await response.json(); // parse the data from string to javascript array
-    setUsers(data); // set the data to the state}
-  }
-
   return (
     <main className="app">
-      <h1>Users</h1>
-      <section className="grid">
-        {users.map(user => (
-          <User key={user.id} name={user.name} image={user.image} title={user.title} mail={user.mail} />
-        ))}
-      </section>
+      <Route path="/" element={<HomePage />}></Route>
     </main>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import User from "./components/User";
+
+export default function HomePage() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/cederdorff/race/master/data/users.json"
+    ); // fetch the data from the API
+    const data = await response.json(); // parse the data from string to javascript array
+    setUsers(data); // set the data to the state}
+  }
+
+  return (
+    <main className="app">
+      <h1>Users</h1>
+      <section className="grid">
+        {users.map((user) => (
+          <User key={user.id} user={user} />
+        ))}
+      </section>
+    </main>
+  );
+}
